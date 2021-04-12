642922315fb9fc06bd0452775a448ec154d11d93
adriandelgg <adriandelgadoc1@gmail.com>
2021-04-11 17:25:32 -0700

    Fixed mobile menu bug

--- src/components/header/MenuMobile.tsx
+++ src/components/header/MenuMobile.tsx
@@ -1,25 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import NavItemsMobile from './NavItemsMobile';
 import Hamburger from 'hamburger-react';
 
 const MenuMobile = () => {
-	const [isOpen, setOpen] = useState(false);
+	const [isOpen, setOpen] = useState<boolean>(false);
 
-	const handleToggle = (toggle: boolean) => {
-		setOpen(toggle);
-		document.body.style.overflow = toggle ? 'hidden' : 'visible';
-		//Find out how to access <main>
-	};
+	useEffect(() => {
+		if (isOpen) {
+			document.body.style.overflow = 'hidden';
+		} else {
+			document.body.style.overflow = 'visible';
+		}
+	});
 
 	return (
 		<>
 			<Hamburger
 				color="#89ddff"
 				label="Navigation Menu"
-				onToggle={handleToggle}
+				toggled={isOpen}
+				toggle={setOpen}
 			/>
-			{isOpen && <NavItemsMobile />}
+			{isOpen && <NavItemsMobile setOpen={setOpen} />}
 		</>
 	);
 };
--- src/components/header/NavItemsMobile.tsx
+++ src/components/header/NavItemsMobile.tsx
@@ -1,35 +1,47 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
+interface Props {
+	setOpen: (isOpen: boolean) => void;
+}
 
-const NavItemMobile = () => {
+const NavItemMobile = ({ setOpen }: Props) => {
+	const handleClick = () => {
+		setOpen(false);
+	};
+
 	return (
 		<ul className="nav-links">
 			<li>
-				<a className="nav-link" href="#about-me">
+				<a onClick={handleClick} className="nav-link" href="#about-me">
 					.about()
 				</a>
 			</li>
 
 			<li>
-				<a className="nav-link" href="#skills">
+				<a onClick={handleClick} className="nav-link" href="#skills">
 					.skills()
 				</a>
 			</li>
 
 			<li>
-				<a className="nav-link" href="#projects">
+				<a onClick={handleClick} className="nav-link" href="#projects">
 					.projects()
 				</a>
 			</li>
 
 			<li>
-				<a className="nav-link" href="#contact">
+				<a onClick={handleClick} className="nav-link" href="#contact">
 					.contact()
 				</a>
 			</li>
 
 			<li>
-				<Button className="nav-link" href="resume.pdf" variant="outlined">
+				<Button
+					onClick={handleClick}
+					className="nav-link"
+					href="resume.pdf"
+					variant="outlined"
+				>
 					Resume
 				</Button>
 			</li>
