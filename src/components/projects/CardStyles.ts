a27251efd3f7842cde23666e5982f4f44478d50c
adriandelgg <adriandelgadoc1@gmail.com>
2021-10-03 00:34:04 -0700

    Hopefully fixed

--- src/components/projects/CardStyles.ts
+++ src/components/projects/CardStyles.ts
@@ -1,11 +1,14 @@
 import { makeStyles } from '@material-ui/core';
 
-const useStyles = makeStyles({
-	root: {
-		backgroundColor: '#181c3a',
-		color: 'white',
-		marginBottom: '2.8em'
-	}
-});
+const useStyles = makeStyles(
+	{
+		root: {
+			backgroundColor: '#181c3a',
+			color: 'white',
+			marginBottom: '2.8em'
+		}
+	},
+	{ index: 1 }
+);
 
 export default useStyles;
