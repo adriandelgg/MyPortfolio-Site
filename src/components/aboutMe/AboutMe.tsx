d2d2b7ce98a432475d513be18cdf358af7a73f52
adriandelgg <adriandelgadoc1@gmail.com>
2023-03-06 16:29:47 -0600

    Fixed lint error

--- src/components/aboutMe/AboutMe.tsx
+++ src/components/aboutMe/AboutMe.tsx
@@ -5,31 +5,32 @@ export const AboutMe = () => {
 			<div className="about-me-section">
 				<div>
 					<p className="about-me-text">
-						Hi there! My name is Adrian Delgado, and I'm a Full-Stack Blockchain
-						& Web Developer with a passion for creating secure and optimized
-						smart contracts on EVM compatible blockchains. I have extensive
-						experience with Solidity, React, Hardhat, TypeScript, MongoDB,
-						Ethers.js, and Node.js, which allows me to create full-stack
-						decentralized applications from start to finish.
+						Hi there! My name is Adrian Delgado, and I&apos;m a Full-Stack
+						Blockchain & Web Developer with a passion for creating secure and
+						optimized smart contracts on EVM compatible blockchains. I have
+						extensive experience with Solidity, React, Hardhat, TypeScript,
+						MongoDB, Ethers.js, and Node.js, which allows me to create
+						full-stack decentralized applications from start to finish.
 					</p>
 					<p className="about-me-text">
 						One of my core strengths is my ability to ensure the security and
-						stability of the applications I create. I'm well-versed in
+						stability of the applications I create. I&apos;m well-versed in
 						industry-standard testing methodologies and thoroughly test my
 						applications to ensure they meet the highest standards of security.
 					</p>
 					<p className="about-me-text">
 						I take pride in my ability to create user-friendly and intuitive
 						interfaces that allow users to seamlessly interact with the
-						applications I build. I'm always exploring new and innovative ways
-						to push the boundaries of what's possible with blockchain technology
-						and am excited to continue building cutting-edge applications that
-						have a real-world impact.
+						applications I build. I&apos;m always exploring new and innovative
+						ways to push the boundaries of what&apos;s possible with blockchain
+						technology and am excited to continue building cutting-edge
+						applications that have a real-world impact.
 					</p>
 					<p className="about-me-text">
-						I'm always looking for new challenges and opportunities to learn and
-						grow as a developer. If you're looking for a passionate, driven
-						developer to bring your ideas to life, let's chat!
+						I&apos;m always looking for new challenges and opportunities to
+						learn and grow as a developer. If you&apos;re looking for a
+						passionate, driven developer to bring your ideas to life, let&apos;s
+						chat!
 					</p>
 				</div>
 				<img src="/adrian.png" alt="" />
