1fda444dcc10396bda9ef18644385c2929e54518
adriandelgg <adriandelgadoc1@gmail.com>
2023-03-06 16:19:18 -0600

    More

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import MenuMobile from './MenuMobile';
-import IsMobile from '../../helper/useMobile';
-import NavItems from './NavItems';
-
-const Navbar = () => {
-	const { isMobile } = IsMobile();
-
-	return <nav>{!isMobile ? <NavItems /> : <MenuMobile />}</nav>;
-};
-
-export default Navbar;
-=======
 import { MenuMobile } from './MenuMobile';
 import { useMobile } from '../../helper/useMobile';
 import { NavItems } from './NavItems';
@@ -20,4 +7,3 @@ export const Navbar = () => {
 
 	return <nav>{!isMobile ? <NavItems /> : <MenuMobile />}</nav>;
 };
->>>>>>> master
