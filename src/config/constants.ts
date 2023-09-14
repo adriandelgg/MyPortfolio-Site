613868af2af3badbcc345e81de278ff82d5384da
codeonblocks <adriandelgadoc1@gmail.com>
2023-09-14 13:10:09 -0500

    Updated LinkedIn link

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
 export const GITHUB_URL = "https://github.com/adriandelgg";
-export const LINKEDIN_URL = "https://www.linkedin.com/in/adriandelgado1/";
+export const LINKEDIN_URL = "https://www.linkedin.com/in/adriandelgg";
 export const EMAIL_URL = "adriandelgado.tech@gmail.com";
 export const RESUME_URL = "";
