60fe6049f1cb36d6777ab290fb6c2488739988a6
adriandelgg <adriandelgadoc1@gmail.com>
2024-05-06 19:02:07 -0500

    Fixed Intro and added React to footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -13,6 +13,10 @@ export const Footer = () => (
     <h5 className="mt-3 text-center">
       © Created by Adrian Delgado {new Date().getFullYear()}
     </h5>
+    <div className="mt-3 flex items-center justify-center">
+      <h5 className="mr-2">Built with React</h5>
+      <img src="/react.svg" alt="React Logo" className="h-5" />
+    </div>
     <div
       className="mx-auto mb-2 mt-8 w-fit cursor-pointer text-center"
       onClick={() =>
